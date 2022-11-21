8f2e59d3ad2f7aba8c95d6b95a417617df255098
Denghao Li <lidhrandom@gmail.com>
2022-11-21 20:57:22 +0800

    fix typo

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 echo "bash test.sh work_dir sgx|direct actinn_dir data_dir result_dir"
 export HDF5_USE_FILE_LOCKING="FALSE"
 cd $1
-gramine-$2 ./python $3/ACTINN-Pytorch/classify.py \
+gramine-$2 ./python $3/ACTINN-PyTorch/classify.py \
     --predict $4/test/test.h5 \
     --result $5/result.csv \
     --pretrained $5/LASTmodel_epoch_5_iter_0.pth
-gramine-$2 ./python $3/ACTINN-Pytorch/evaluate.py $5/result.csv $5/test_lab.csv
+gramine-$2 ./python $3/ACTINN-PyTorch/evaluate.py $5/result.csv $5/test_lab.csv
--- train.sh
+++ train.sh
@@ -1,4 +1,6 @@
-echo "bash train.sh work_dir sgx|direct actinn_dir data_dir result_dir"
+echo "sh train.sh work_dir sgx|direct actinn_dir data_dir result_dir"
+echo "e.g. sh train.sh code/test_steps sgx /workspace workspace/data/encrypted /workspace"
 export HDF5_USE_FILE_LOCKING="FALSE"
 cd $1
-gramine-$2 ./python $3/ACTINN-Pytorch/classify.py --data_prefix $4 --model_dir $5
+chown -R root:root /$4/
+gramine-$2 ./python $3/ACTINN-PyTorch/classify.py --data_prefix $4 --model_dir $5
