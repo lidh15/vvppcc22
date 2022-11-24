b0a17520a5e35cad0733bf53516b6e43223d7c95
Denghao Li <lidhrandom@gmail.com>
2022-11-24 18:22:21 +0800

    fix path

--- guest.after.sh
+++ guest.after.sh
@@ -1,4 +1,9 @@
 cd /workspace/vvppcc22-master/results
 sh pf-decrypt.sh
 cd ..
-sh test.sh . sgx /workspace workspace/vvppcc22-master/data /workspace/vvppcc22-master/results
+if [ -f data/test.csv ]; then
+    path=
+else
+    path=/test_steps
+fi
+sh test.guest.sh . sgx /workspace workspace/vvppcc22-master/data$path /workspace/vvppcc22-master/results
