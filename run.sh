aab2f94fade0c93efbd8cc25aa59ceccffde11de
Denghao Li <lidhrandom@gmail.com>
2022-11-28 21:01:40 +0800

    add start and run

---
+++ run.sh
@@ -0,0 +1,6 @@
+docker exec GUEST sh -c "cd /workspace && sh guest.before.sh >> GUEST.log"
+docker exec HOST sh -c "cd /workspace && sh host.sh >> HOST.log"
+docker exec GUEST sh -c "cd /workspace && sh guest.after.sh >> GUEST.log"
+echo "workflow finished."
+echo "please check GUEST.log and HOST.log for log and evaluation results"
+echo "please check `pwd`/vvppcc22-master/results/result.csv for prediction results"
---
+++ start.sh
@@ -0,0 +1,45 @@
+echo -e "current directory `pwd`"
+if [ -f code.tgz ]; then
+    echo "code.tgz detected"
+else
+    echo "please put code.tgz in current directory"
+    exit 0
+fi
+
+if [ -f wppcc2022.tgz ]; then
+    echo "wppcc2022.tgz detected"
+else
+    echo "please put wppcc2022.tgz in current directory"
+    exit 0
+fi
+
+if [ -f /data/tee/test.csv ]; then
+    echo "/data/tee/test.csv detected"
+else
+    echo "please put test.csv in /data/tee"
+    exit 0
+fi
+
+if [ -f /data/tee/test_lab.csv ]; then
+    echo "/data/tee/test_lab.csv detected"
+else
+    echo "test_lab.csv not detected in /data/tee"
+    echo "please put test_lab.csv in /data/tee if you need auto evaluation"
+    echo "creating empty test_lab.csv..."
+    n=`wc -l /data/tee/test.csv`
+    m=$((${n% *} - 1))
+    t=""
+    for i in `seq $m`; do t=$t"\t\n"; done
+    echo -e $t > /data/tee/test_lab.csv
+fi
+
+tar zxvf code.tgz
+unzip ACTINN-PyTorch-main.zip
+mv ACTINN-PyTorch-main ACTINN-PyTorch
+unzip vvppcc22-master.zip
+cp /data/tee/test.csv vvppcc22-master/data
+cp /data/tee/test_lab.csv vvppcc22-master/data/test
+docker load -i wppcc2022.tgz
+docker run --rm -it -v `pwd`:/workspace --device /dev/sgx_enclave --device /dev/sgx_provision --name HOST -d wppcc2022 /bin/bash
+docker run --rm -it -v `pwd`:/workspace --name GUEST -d wppcc2022 /bin/bash
+docker ps
