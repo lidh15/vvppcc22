b20e54b436238b4562e5aa46978fb4c3e344481e
Denghao Li <lidhrandom@gmail.com>
2022-11-29 18:56:26 +0800

    sample test

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,14 +1,22 @@
 import pandas as pd
 import os
+from sklearn.model_selection import train_test_split
 
 target = "./"
 raw = pd.read_csv("train.csv")
+_, test = train_test_split(raw, train_size=0.9)
 
 y = raw["label"].tolist()
 X = raw.drop(columns=["label"]).transpose()
 with open(f"{target}train_lab.csv", "w") as f:
     f.writelines([f"{k}\t{j}\n" for k, j in enumerate(y)])
 X.to_hdf(f"{target}train.h5", key='dge', mode='w', complevel=3)
+
+y = test["label"].tolist()
+X = test.drop(columns=["label"]).transpose()
+with open(f"{target}test_lab.csv", "w") as f:
+    f.writelines([f"{k}\t{j}\n" for k, j in enumerate(y)])
+X.to_hdf(f"{target}test.h5", key='dge', mode='w', complevel=3)
 
 if "test.csv" in os.listdir(target):
     print("test.csv found!")
--- data/preprocess.sh
+++ data/preprocess.sh
@@ -9,5 +9,5 @@ echo "copying"
 # if [ -f test.csv ]; then
 #     mv test.h5 test/
 # fi
-cp train.h5 test.h5
-cp train_lab.csv test_lab.csv
+# cp train.h5 test.h5
+# cp train_lab.csv test_lab.csv
